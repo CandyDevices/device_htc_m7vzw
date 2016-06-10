600c97d761d9ba82176d66b72a163aa90532f443
Matt Mower <mowerm@gmail.com>
2016-06-10 14:08:18 -0500

    m7vzw: Clean up makefile inheritance/ordering
    * Clean up candy.mk, device.mk, and full_m7vzw.mk
    
    Change-Id: I6ab38beaa7c632b2866745aaf07cd159fc12dcb2
    
    Conflicts:
    	candy.mk

--- candy.mk
+++ candy.mk
@@ -4,13 +4,15 @@ $(call inherit-product, vendor/candy/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/m7vzw/full_m7vzw.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=HTCOneVZW \
-    BUILD_ID=LRX22G \
-    BUILD_FINGERPRINT="htc/HTCOneVZW/m7wlv:5.0.2/LRX22G/495599.6:user/release-keys" \
-    PRIVATE_BUILD_DESC="6.22.605.6 CL495599 release-keys"
-
-PRODUCT_GMS_CLIENTID_BASE := android-verizon
-
+# Device naming
 PRODUCT_NAME := candy_m7vzw
-PRODUCT_DEVICE := m7vzw
+
+# Override build props
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="htc/HTCOneVZW/m7wlv:5.0.2/LRX22G/495599.6:user/release-keys" \
+    BUILD_ID=LRX22G \
+    PRIVATE_BUILD_DESC="6.22.605.6 CL495599 release-keys" \
+    PRODUCT_NAME=HTCOneVZW
+
+# Override ro.com.google.clientidbase
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
